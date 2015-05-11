884434978503a17a1d997f5ac69157922767534b
Alexander01998 <contact.wurstimperium@gmail.com>
2015-05-11 23:02:18 +0200

    Add class name to hook() call

--- Hook Manager/src/tk/wurst_client/hooks/ClassHookInjector.java
+++ Hook Manager/src/tk/wurst_client/hooks/ClassHookInjector.java
@@ -1,6 +1,6 @@
 /*
  * Copyright © 2015 | Alexander01998 | All rights reserved.
- * 
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -14,21 +14,31 @@ import org.objectweb.asm.MethodVisitor;
 public class ClassHookInjector extends ClassVisitor
 {
 	private int api;
-	
+	private String className;
+
 	public ClassHookInjector(int api, ClassWriter cv)
 	{
 		super(api, cv);
 		this.api = api;
 	}
-	
+
+	@Override
+	public void visit(int version, int access, String name, String signature,
+		String superName, String[] interfaces)
+	{
+		super.visit(version, access, name, signature, superName, interfaces);
+		className = name;
+	}
+
 	@Override
 	public MethodVisitor visitMethod(int access, String name, String desc,
 		String signature, String[] exceptions)
 	{
-		
+
 		MethodVisitor mv =
 			super.visitMethod(access, name, desc, signature, exceptions);
-		MethodHookInjector mvw = new MethodHookInjector(api, mv, name);
+		MethodHookInjector mvw =
+			new MethodHookInjector(api, mv, className, name);
 		return mvw;
 	}
 }
--- Hook Manager/src/tk/wurst_client/hooks/MethodHookInjector.java
+++ Hook Manager/src/tk/wurst_client/hooks/MethodHookInjector.java
@@ -13,19 +13,20 @@ import org.objectweb.asm.Opcodes;
 public class MethodHookInjector extends MethodVisitor
 {
 	private String methodName;
+	private String className;
 	
-	public MethodHookInjector(int api, MethodVisitor mv, String methodName)
+	public MethodHookInjector(int api, MethodVisitor mv, String className, String methodName)
 	{
 		super(api, mv);
 		this.methodName = methodName;
+		this.className = className;
 	}
 	
 	@Override
 	public void visitCode()
 	{
 		super.visitCode();
-		super.visitLdcInsn(methodName);
-		// TODO: Class name + method name
+		super.visitLdcInsn(className + "." + methodName);
 		super.visitMethodInsn(Opcodes.INVOKESTATIC,
 			"tk/wurst_client/hooks/HookManager" /* TODO: Custom class path */,
 			"hook", "(Ljava/lang/String;)V", false);
