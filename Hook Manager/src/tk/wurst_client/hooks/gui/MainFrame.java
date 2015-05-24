fe49d7a0fb087d8962ed87733f9947ce191adb59
Alexander01998 <contact.wurstimperium@gmail.com>
2015-05-24 16:35:14 +0200

    Change editor to field

--- Hook Manager/src/tk/wurst_client/hooks/gui/MainFrame.java
+++ Hook Manager/src/tk/wurst_client/hooks/gui/MainFrame.java
@@ -32,6 +32,7 @@ public class MainFrame extends JFrame
 	private JTree tree;
 	private JarDataReader jarDataReader;
 	private JarData settings;
+	private HTMLPanel editor;
 	
 	/**
 	 * Launch the application.
@@ -258,7 +259,7 @@ public class MainFrame extends JFrame
 		jarDataReader = new JarDataReader(tree);
 		scrollPane.setViewportView(tree);
 		
-		HTMLPanel editor = new HTMLPanel();
+		editor = new HTMLPanel();
 		editor.setHTMLFile("editor-nojar.html");
 		editor.setBridge(new EditorBridge());
 		splitPane.setRightComponent(editor);
