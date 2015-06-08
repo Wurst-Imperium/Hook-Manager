7e18b4bfe126ba2024d3368d2478bbfbb8320b15
Alexander01998 <contact.wurstimperium@gmail.com>
2015-06-08 15:47:19 +0200

    Make all methods public
    Example:
    private void doStuff() > public void doStuff()

--- Hook Manager/src/tk/wurst_client/hooks/injector/ClassHookInjector.java
+++ Hook Manager/src/tk/wurst_client/hooks/injector/ClassHookInjector.java
@@ -1,6 +1,6 @@
 /*
  * Copyright © 2015 | Alexander01998 | All rights reserved.
- *
+ * 
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -9,6 +9,7 @@ package tk.wurst_client.hooks.injector;
 
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
 
 import tk.wurst_client.hooks.reader.data.ClassData;
 
@@ -17,14 +18,14 @@ public class ClassHookInjector extends ClassVisitor
 	private int api;
 	private String className;
 	private ClassData classData;
-
+	
 	public ClassHookInjector(int api, ClassVisitor cv, ClassData classData)
 	{
 		super(api, cv);
 		this.api = api;
 		this.classData = classData;
 	}
-
+	
 	@Override
 	public void visit(int version, int access, String name, String signature,
 		String superName, String[] interfaces)
@@ -32,16 +33,19 @@ public class ClassHookInjector extends ClassVisitor
 		super.visit(version, access, name, signature, superName, interfaces);
 		className = name;
 	}
-
+	
 	@Override
 	public MethodVisitor visitMethod(int access, String name, String desc,
 		String signature, String[] exceptions)
 	{
-
+		access =
+			access & ~Opcodes.ACC_PRIVATE & ~Opcodes.ACC_PROTECTED
+				| Opcodes.ACC_PUBLIC;
 		MethodVisitor mv =
 			super.visitMethod(access, name, desc, signature, exceptions);
 		if(classData.hasHooks() && classData.getMethod(name + desc).hasHooks())
-			return new MethodHookInjector(api, mv, classData.getMethod(name + desc), className, name);
+			return new MethodHookInjector(api, mv, classData.getMethod(name
+				+ desc), className, name);
 		else
 			return mv;
 	}
